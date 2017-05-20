cf4c186bc79b8ad4061325b2fc459d19f51c9594
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-05-20 16:19:23 +0300

    keybord control

--- core/src/main/java/com/tmc/clutterspace/core/Main.java
+++ core/src/main/java/com/tmc/clutterspace/core/Main.java
@@ -1,20 +1,30 @@
 package com.tmc.clutterspace.core;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+import java.util.Random;
 
 public class Main implements ApplicationListener {
-	Texture texture;
+	Texture texture,texture2;
 	SpriteBatch batch;
 	float elapsed;
+	private Sprite sprite ,sprite2;
 
 	@Override
 	public void create () {
-		texture = new Texture(Gdx.files.internal("libgdx-logo.png"));
+		texture = new Texture(Gdx.files.internal("lion.png"));
+		texture2 = new Texture(Gdx.files.internal("pidgey.png"));
 		batch = new SpriteBatch();
+		sprite = new Sprite(texture);
+		sprite2 = new Sprite(texture2);
+
+
 	}
 
 	@Override
@@ -22,12 +32,70 @@ public class Main implements ApplicationListener {
 	}
 
 	@Override
-	public void render () {
-		elapsed += Gdx.graphics.getDeltaTime();
-		Gdx.gl.glClearColor(0, 0, 0, 0);
+	public void render() {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateX(-1f);
+			else
+				sprite.translateX(-10.0f);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateX(1f);
+			else
+				sprite.translateX(10.0f);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateY(1f);
+			else
+				sprite.translateY(10.0f);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+				sprite.translateY(-1f);
+			else
+				sprite.translateY(-10.0f);
+		}
+        if(Gdx.input.isKeyPressed(Input.Keys.R)){
+            sprite.setPosition(50, 50);
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.A)){
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                sprite2.translateX(-1f);
+            else
+                sprite2.translateX(-10.0f);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.D)){
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                sprite2.translateX(1f);
+            else
+                sprite2.translateX(10.0f);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.W)){
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                sprite2.translateY(1f);
+            else
+                sprite2.translateY(10.0f);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.S)){
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                sprite2.translateY(-1f);
+            else
+                sprite2.translateY(-10.0f);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.E)){
+            sprite2.setPosition(50, 50);
+        }
+
 		batch.begin();
-		batch.draw(texture, 100+100*(float)Math.cos(elapsed), 100+25*(float)Math.sin(elapsed));
+
+		sprite.draw(batch);
+        sprite2.draw(batch);
 		batch.end();
 	}
 
