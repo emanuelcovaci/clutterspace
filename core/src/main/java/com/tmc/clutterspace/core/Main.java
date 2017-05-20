898c0c5a1575ee74cfdd048954cfa294661cc0e4
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-05-20 17:19:19 +0300

    game

--- core/src/main/java/com/tmc/clutterspace/core/Main.java
+++ core/src/main/java/com/tmc/clutterspace/core/Main.java
@@ -1,113 +1,46 @@
 package com.tmc.clutterspace.core;
 
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.util.Random;
 
-public class Main implements ApplicationListener {
-	Texture texture,texture2;
+public class Main extends Game {
 	SpriteBatch batch;
-	float elapsed;
-	private Sprite sprite ,sprite2;
+    OrthographicCamera cam;
+    Viewport viewport;
 
 	@Override
 	public void create () {
-		texture = new Texture(Gdx.files.internal("lion.png"));
-		texture2 = new Texture(Gdx.files.internal("pidgey.png"));
-		batch = new SpriteBatch();
-		sprite = new Sprite(texture);
-		sprite2 = new Sprite(texture2);
-
-
+        cam = new OrthographicCamera(800, 600);
+        viewport = new StretchViewport(800, 600, cam);
+	    batch = new SpriteBatch();
+	    resize(800, 600);
+        this.setScreen(new FirstScreen(this));
 	}
 
-	@Override
-	public void resize (int width, int height) {
-	}
+	public void resize(int w, int h){
+        if (!Gdx.app.getType().equals(Application.ApplicationType.Android)) {
+            Gdx.graphics.setDisplayMode(w, h, false);
+            viewport.update(w, h, true);
+        } else {
+            Gdx.graphics.setDisplayMode(Gdx.graphics.getWidth(),Gdx.graphics.getHeight(), true);
+        }
+    }
 
-	@Override
 	public void render() {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+	    super.render();
 
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-				sprite.translateX(-1f);
-			else
-				sprite.translateX(-10.0f);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-				sprite.translateX(1f);
-			else
-				sprite.translateX(10.0f);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-				sprite.translateY(1f);
-			else
-				sprite.translateY(10.0f);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-				sprite.translateY(-1f);
-			else
-				sprite.translateY(-10.0f);
-		}
-        if(Gdx.input.isKeyPressed(Input.Keys.R)){
-            sprite.setPosition(50, 50);
-        }
-
-        if(Gdx.input.isKeyPressed(Input.Keys.A)){
-            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                sprite2.translateX(-1f);
-            else
-                sprite2.translateX(-10.0f);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.D)){
-            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                sprite2.translateX(1f);
-            else
-                sprite2.translateX(10.0f);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.W)){
-            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                sprite2.translateY(1f);
-            else
-                sprite2.translateY(10.0f);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.S)){
-            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-                sprite2.translateY(-1f);
-            else
-                sprite2.translateY(-10.0f);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.E)){
-            sprite2.setPosition(50, 50);
-        }
-
-		batch.begin();
-
-		sprite.draw(batch);
-        sprite2.draw(batch);
-		batch.end();
 	}
 
-	@Override
-	public void pause () {
-	}
-
-	@Override
-	public void resume () {
-	}
-
-	@Override
-	public void dispose () {
-	}
+    public void dispose() {
+        batch.dispose();
+    }
 }
