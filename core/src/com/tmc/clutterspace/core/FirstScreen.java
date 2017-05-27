d426c3c21aa0a812a827d7ce738776c3748c4d15
Emanuel Covaci <covaciemanuel98@gmail.com>
2017-05-27 11:36:44 +0300

    deleted old control from fristscreen

--- core/src/com/tmc/clutterspace/core/FirstScreen.java
+++ core/src/com/tmc/clutterspace/core/FirstScreen.java
@@ -112,61 +112,6 @@ public class FirstScreen implements Screen {
         music_level1.setLooping(true);
         music_level1.play();
         w.step(5/60f, 6, 2);
-//        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-//            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-//                sprite.translateX(-1f);
-//            else
-//                sprite.translateX(-10.0f);
-//        }
-//        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-//            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-//                sprite.translateX(1f);
-//            else
-//                sprite.translateX(10.0f);
-//        }
-//        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-//            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-//                sprite.translateY(1f);
-//            else
-//                sprite.translateY(10.0f);
-//        }
-//        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-//            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-//                sprite.translateY(-1f);
-//            else
-//                sprite.translateY(-10.0f);
-//        }
-//        if(Gdx.input.isKeyPressed(Input.Keys.R)){
-//            sprite.setPosition(50, 50);
-//        }
-//
-//        if(Gdx.input.isKeyPressed(Input.Keys.A)){
-//            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-//                sprite2.translateX(-1f);
-//            else
-//                sprite2.translateX(-10.0f);
-//        }
-//        if(Gdx.input.isKeyPressed(Input.Keys.D)){
-//            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-//                sprite2.translateX(1f);
-//            else
-//                sprite2.translateX(10.0f);
-//        }
-//        if(Gdx.input.isKeyPressed(Input.Keys.W)){
-//            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-//                sprite2.translateY(1f);
-//            else
-//                sprite2.translateY(10.0f);
-//        }
-//        if(Gdx.input.isKeyPressed(Input.Keys.S)){
-//            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
-//                sprite2.translateY(-1f);
-//            else
-//                sprite2.translateY(-10.0f);
-//        }
-//        if(Gdx.input.isKeyPressed(Input.Keys.E)){
-//            sprite2.setPosition(50, 50);
-//        }
 
         game.batch.setProjectionMatrix(game.cam.combined);
         game.batch.begin();
